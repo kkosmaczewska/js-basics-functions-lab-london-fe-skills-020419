a0537438c8628137df36fbea993dffe1de842cad
Kasia Kosmaczewska <kakosmaczewska@gmail.com>
2019-03-18 18:49:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function calculatesFarePrice(start,destination) {
   if (Math.abs(start-destination)===0) {
     return 'gives customers a free sample'
   }
- else if bs(start-destination)=>2000) {
+ else if Math.abs(start-destination)=>2000) {
     return 'charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!)'
   }
 }
