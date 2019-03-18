a2e9fe7bb6cdd2fb6451766575176e8e87a1c086
Kasia Kosmaczewska <kakosmaczewska@gmail.com>
2019-03-18 18:56:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ function distanceTravelledInFeet(a,b) {
   return Math.abs(a-b)*264;
 }
 function calculatesFarePrice(start,destination) {
-  if (Math.abs(start-destination)===0) {
+  var distance = distanceTravelledInFeet(start,destination)
+  if (distance===0) {
     return 'gives customers a free sample'
   }
- else if (Math.abs(start-destination)=>2000) {
+ else if (distance=>2000) {
     return 'charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!)'
   }
-  else if (Math.abs(start-destination)=>2500) {
-    return ''
+  else if (distance=>2500) {
+    return 'charges 25 dollars for a distance over 2000 feet'
   }
 }
