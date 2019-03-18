a6a806ab171d2ae11211eede7f5beabfe3c633c3
Kasia Kosmaczewska <kakosmaczewska@gmail.com>
2019-03-18 18:41:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+const hqBlock=42;
+
+function distanceFromHqInBlocks(block) {
+return Math.abs(block-hqBlock);
+}
+function distanceFromHqInFeet(feet) {
+  return distanceFromHqInBlocks(feet)*264;
+}
+function distanceTravelledInFeet(a,b) {
+  return Math.abs(a-b)*264;
+}
+function calculatesFarePrice(start,destination) {
+  if Math.abs(start-destination)=0 {
+    return 'gives customers a free sample'
+  }
+  if Math.abs(start-destination)=
+}
